80bfa4167bd180c8586115dadbf84c9ea4bcc092
lasdia <l.diallo0@laposte.net>
2021-10-08 00:01:28 +0200

    creation commentaire

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommentaireController extends AbstractController
 {
-    /**
-     * @Route("/commentaire", name="commentaire")
+      /**
+     * @Route("/commentaire", name="new_commentaire")
      */
-    public function index(): Response
-    {
-        return $this->render('commentaire/index.html.twig', [
-            'controller_name' => 'CommentaireController',
-        ]);
+    public function Create(Request $request, EntityManagerInterface $em): Response{
+
+        $commentaire = new Commentaire;
+
+        $formulaire = $this->createForm(CommentaireType::class, $commentaire);
+
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()){
+
+            $em->persist($commentaire);
+            $em->flush();
+
+            return $this->redirectToRoute('accueil');
+        }else{
+            return $this->render('commentaire/formulaire.html.twig',[
+                'type' => 'Ajouter',
+                'formulaire' => $formulaire->createView()
+    
+            ]);
+        }
+
+    }
+
+       /**
+     * @Route("/commentaire/{id}/delete", name="delete_commentaire")
+     */
+    public function delete(Commentaire $commentaire, EntityManagerInterface $em){
+        $em->remove($commentaire);
+        $em->flush();
+
+        return $this->redirectToRoute('accueil');
     }
 }
--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -2,21 +2,43 @@
 
 namespace App\Form;
 
+use App\Entity\Article;
 use App\Entity\Commentaire;
+use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CommentaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contenu')
-            ->add('datePublication')
-            ->add('article')
-            ->add('utilisateur')
-        ;
+            ->add('contenu', TextType::class,[
+                'label' => 'contenu'
+            ])
+            ->add('datePublication', DateType::class,[
+                'label' => 'date'
+            ])
+            ->add('article', EntityType::class,[
+                'class'=> Article::class,
+                'choice_label'=> 'article',
+                'required' => false
+            ])
+            ->add('utilisateur', EntityType::class,[
+                'class'=> Utilisateur::class,
+                'choice_label'=> 'pseudo',
+                'required' => false
+             
+            ])
+            
+            ->add('submit', SubmitType::class,[
+                'label'=> 'Envoyer'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
